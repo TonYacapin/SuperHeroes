fc6a43213783e055066e1b4986e3adc7971eff5e
AngelHameltonYacapin <yacapinton@gmail.com>
2024-04-01 17:25:18 +0800

    Second Commit

--- app/src/main/java/com/example/cc10624superhero/MainActivity.java
+++ app/src/main/java/com/example/cc10624superhero/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cc10624superhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +29,34 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,items);
         lvHero.setAdapter(adapter);
 
+
+        lvHero.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String nameHero = items[position];
+
+                Intent intent = new Intent(MainActivity.this, ShowHero.class);
+
+                Bundle bundle = new Bundle();
+
+
+
+                bundle.putString("heroname", nameHero);
+                bundle.putInt("herono",position);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+
+
+
+
+
+
+
+
+
+            }
+        });
 
 
 
--- app/src/main/java/com/example/cc10624superhero/ShowHero.java
+++ app/src/main/java/com/example/cc10624superhero/ShowHero.java
@@ -2,13 +2,28 @@ package com.example.cc10624superhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShowHero extends AppCompatActivity {
+
+
+    private TextView lblHeroName;
+    private ImageView imgHero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_hero);
+
+
+
+        lblHeroName = (TextView)  findViewById(R.id.lblHeroName);
+        imgHero = (ImageView) findViewById(R.id.imgHero);
+
+
+
     }
 }
