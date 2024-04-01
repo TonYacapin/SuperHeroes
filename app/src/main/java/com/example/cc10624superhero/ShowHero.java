108367cb2674e17ff90e9fc946530644cb333e3f
AngelHameltonYacapin <yacapinton@gmail.com>
2024-04-01 17:36:07 +0800

    Third Commit

--- app/src/main/java/com/example/cc10624superhero/ShowHero.java
+++ app/src/main/java/com/example/cc10624superhero/ShowHero.java
@@ -2,6 +2,7 @@ package com.example.cc10624superhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -22,6 +23,14 @@ public class ShowHero extends AppCompatActivity {
 
         lblHeroName = (TextView)  findViewById(R.id.lblHeroName);
         imgHero = (ImageView) findViewById(R.id.imgHero);
+
+
+        Intent intent = getIntent();
+        String heroname = "Hero is " + intent.getExtras().getString("heroname");
+        lblHeroName.setText(heroname);
+
+        int heroindex = intent.getExtras().getInt("herono");
+        imgHero.setImageResource(getResources().getIdentifier("hero" + heroindex, "drawable", getPackageName()));
 
 
 
